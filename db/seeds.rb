5b3c17eb8f69463fa5767cff3b6b2ebe5567c337
Noel Delgado <noeldelgadom@gmail.com>
2018-07-02 23:40:16 -0500

    added some bootstrap styles so it doesnt look horrible

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@ Condition.create!(name: 'Allergies',      increase: 1)
 Condition.create!(name: 'Sleep Apnea',    increase: 6)
 Condition.create!(name: 'Heart Disease',  increase: 17)
 
-Client.create!( name:         'Kelly',
-                age:          50,
-                gender_id:    2,
-                condition_id: 2)
+# Client.create!( name:         'Kelly',
+#                 age:          50,
+#                 gender_id:    2,
+#                 condition_id: 2)
 
-Client.create!( name:         'Josh',
-                age:          40,
-                gender_id:    1,
-                condition_id: 3)
+# Client.create!( name:         'Josh',
+#                 age:          40,
+#                 gender_id:    1,
+#                 condition_id: 3)
 
-Client.create!( name:         'Brad',
-                age:          20,
-                gender_id:    1,
-                condition_id: 4)
+# Client.create!( name:         'Brad',
+#                 age:          20,
+#                 gender_id:    1,
+#                 condition_id: 4)
